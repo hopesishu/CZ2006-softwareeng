5592849eefe1f0235d0cdc7ac0e6f58a4e296f44
hopesishu <hopesishu@gmail.com>
2020-11-04 15:56:10 +0800

    updated navigation bar linking

--- app/src/main/java/com/example/connectme/MainActivity.java
+++ app/src/main/java/com/example/connectme/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.connectme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private CardView parkConnectorCard, hawkerCentreCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //linking cards to other activities
+        parkConnectorCard = (CardView) findViewById(R.id.cardParkConnectors);
+        hawkerCentreCard = (CardView) findViewById(R.id.cardHawkerCentres);
+
+        parkConnectorCard.setOnClickListener(this);
+        hawkerCentreCard.setOnClickListener(this);
+
+        //linking bottom navigation bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.navigationHome);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.navigationMap:
+                        //startActivity(new Intent(getApplicationContext(), activity_map.class));
+                        //overridePendingTransition(0,0);
+                        //return true;
+                    case R.id.navigationInfo:
+                        //startActivity(new Intent(getApplicationContext(), activity_info.class));
+                    case R.id.navigationHome:
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch(view.getId()) {
+            case R.id.cardParkConnectors:
+                i = new Intent(this, activity_parkconnector.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardHawkerCentres:
+                i = new Intent(this, activity_hawkercentres.class);
+                startActivity(i);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/connectme/activity_parkconnector.java
+++ app/src/main/java/com/example/connectme/activity_parkconnector.java
@@ -1,16 +1,48 @@
 package com.example.connectme;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
+import android.view.MenuItem;
 import android.view.View;
 
 public class activity_parkconnector extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parkconnector);
 
+        //linking bottom navigation bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.navigationHome);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.navigationMap:
+                        //startActivity(new Intent(getApplicationContext(), activity_map.class));
+                        //overridePendingTransition(0,0);
+                        //return true;
+                    case R.id.navigationInfo:
+                        //startActivity(new Intent(getApplicationContext(), activity_info.class));
+                    case R.id.navigationHome:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
 }
