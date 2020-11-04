5e8b12990df317f39de09e01e0339abea0e5cd34
theturtle27 <62739849+theturtle27@users.noreply.github.com>
2020-11-04 15:22:32 +0800

    profile working but need to know how to interface with db

--- app/src/main/java/com/example/connectme/activity_profile.java
+++ app/src/main/java/com/example/connectme/activity_profile.java
@@ -5,12 +5,27 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import control.ProfileMgr;
+import entity.ProfileMgrInterface;
+
 public class activity_profile extends AppCompatActivity {
+    //vars
+    private ProfileMgrInterface profileMgr;
+    private String uId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        profileMgr = new ProfileMgr();
+        String apple = user.getUid();
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_profile);
-        TextView textView = (TextView) findViewById(R.id.profile_name);
-        textView.setText("gg bro");
+        TextView profile_name = (TextView) findViewById(R.id.profile_name);
+        TextView dateOfBirth = (TextView) findViewById(R.id.date_of_birth);
+
+        //profileMgr.retrieveCurrentProfileName(value -> profile_name.setText(value), uId);
+        profile_name.setText("Name: " + apple);
+        dateOfBirth.setText("Date Of Birth: " + apple);
+
+
     }
 }
