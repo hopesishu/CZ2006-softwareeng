7ac3053a80ef18dcc70851deea60049af464cece
yixingchin <57394787+yixingchin@users.noreply.github.com>
2020-11-10 23:19:00 +0800

    all functions done

--- app/src/main/java/com/example/connectme/SearchActivity.java
+++ app/src/main/java/com/example/connectme/SearchActivity.java
@@ -29,60 +29,58 @@ public class SearchActivity extends AppCompatActivity {
     List<Object> polylineList;
     boolean isHawker = true;
     Random r;
+    MapsActivity mActivity = new MapsActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         String select = getIntent().getStringExtra("Search Option");
-        if (select == "park") {
-
-        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         parkDatabaseList = Park.getParkDatabase();
         ArrayList<ArrayList<String>> listOfLists = new ArrayList<ArrayList<String>>();
-//        if (select == "hawker"){
-//            //call hawker list, listOfLists = function
-//        }
-//        else if (select == "park"){
-//            isHawker = false;
-//            //call park list, listOfLists = function
-//            for (int i=0; i<listOfLists.size(); i++) {
-//                r = new Random();
-//                int ranNum = r.nextInt(20);
-//                listOfLists.get(i).set(2, parkDatabaseList.get(ranNum));
-//            }
-//        }
+        if (select == "hawker"){
+            listOfLists = mActivity.getMapHawker();
+        }
+        else if (select == "park"){
+            isHawker = false;
+            listOfLists = mActivity.getMapPcn();
+            for (int i=0; i<listOfLists.size(); i++) {
+                r = new Random();
+                int ranNum = r.nextInt(20);
+                listOfLists.get(i).set(2, parkDatabaseList.get(ranNum));
+            }
+        }
 
-        ArrayList<String> list1 = new ArrayList<String>();
-        list1.add("Jurong West Hawker center");
-        list1.add("Jurong West st 100");
-        list1.add("https://www.nea.gov.sg/images/default-source/Hawker-Centres-Division/resize_1262155861677.jpg");
-        list1.add("polyline");
-        listOfLists.add(list1);
-
-        ArrayList<String> anotherList = new ArrayList<String>();
-
-        anotherList.add("Taman Jurong Hawker");
-        anotherList.add("Yung Ho Road");
-        anotherList.add("http://www.nea.gov.sg/images/default-source/Hawker-Centres-Division/resize_1267879560483.jpg");
-        anotherList.add("polyline");
-        listOfLists.add(anotherList);
-
-        ArrayList<String> anotherList2 = new ArrayList<String>();
-
-        anotherList2.add("Ellias Mall Hawker");
-        anotherList2.add("Pasir Ris");
-        anotherList2.add("http://www.nea.gov.sg/images/default-source/Hawker-Centres-Division/resize_1262154766447.jpg");
-        anotherList2.add("polyline");
-        listOfLists.add(anotherList2);
-
-        ArrayList<String> anotherList3 = new ArrayList<String>();
-
-        anotherList3.add("Tampines Hawker");
-        anotherList3.add("Tampines");
-        anotherList3.add("http://www.nea.gov.sg/images/default-source/Hawker-Centres-Division/resize_1267846802175.jpg");
-        anotherList3.add("polyline");
-        listOfLists.add(anotherList3);
+//        ArrayList<String> list1 = new ArrayList<String>();
+//        list1.add("Jurong West Hawker center");
+//        list1.add("Jurong West st 100");
+//        list1.add("https://www.nea.gov.sg/images/default-source/Hawker-Centres-Division/resize_1262155861677.jpg");
+//        list1.add("polyline");
+//        listOfLists.add(list1);
+//
+//        ArrayList<String> anotherList = new ArrayList<String>();
+//
+//        anotherList.add("Taman Jurong Hawker");
+//        anotherList.add("Yung Ho Road");
+//        anotherList.add("http://www.nea.gov.sg/images/default-source/Hawker-Centres-Division/resize_1267879560483.jpg");
+//        anotherList.add("polyline");
+//        listOfLists.add(anotherList);
+//
+//        ArrayList<String> anotherList2 = new ArrayList<String>();
+//
+//        anotherList2.add("Ellias Mall Hawker");
+//        anotherList2.add("Pasir Ris");
+//        anotherList2.add("http://www.nea.gov.sg/images/default-source/Hawker-Centres-Division/resize_1262154766447.jpg");
+//        anotherList2.add("polyline");
+//        listOfLists.add(anotherList2);
+//
+//        ArrayList<String> anotherList3 = new ArrayList<String>();
+//
+//        anotherList3.add("Tampines Hawker");
+//        anotherList3.add("Tampines");
+//        anotherList3.add("http://www.nea.gov.sg/images/default-source/Hawker-Centres-Division/resize_1267846802175.jpg");
+//        anotherList3.add("polyline");
+//        listOfLists.add(anotherList3);
 
         nameList = new ArrayList<>();
         addressList = new ArrayList<>();
@@ -101,35 +99,7 @@ public class SearchActivity extends AppCompatActivity {
                 }
             }
         }
-        // add in list for polyline
 
-
-
-
-        moviesList = new ArrayList<>();
-        moviesList.add("Iron Man");
-        moviesList.add("The Incredible Hulk");
-        moviesList.add("Iron Man 2");
-        moviesList.add("Thor");
-        moviesList.add("Captain America: The First Avenger");
-        moviesList.add("The Avengers");
-        moviesList.add("Iron Man 3");
-        moviesList.add("Thor: The Dark World");
-        moviesList.add("Captain America: The Winter Soldier");
-        moviesList.add("Guardians of the Galaxy");
-        moviesList.add("Avengers: Age of Ultron");
-        moviesList.add("Ant-Man");
-        moviesList.add("Captain America: Civil War");
-        moviesList.add("Doctor Strange");
-        moviesList.add("Guardians of the Galaxy Vol. 2");
-        moviesList.add("Spider-Man: Homecoming");
-        moviesList.add("Thor: Ragnarok");
-        moviesList.add("Black Panther");
-        moviesList.add("Avengers: Infinity War");
-        moviesList.add("Ant-Man and the Wasp");
-        moviesList.add("Captain Marvel");
-        moviesList.add("Avengers: Endgame");
-        moviesList.add("Spider-Man: Far From Home");
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerAdapter = new RecyclerAdapter(nameList, addressList, urlList, isHawker);
